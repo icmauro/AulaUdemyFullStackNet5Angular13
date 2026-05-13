4bb5a3f9789092a91e7e34c4f99f9f68ef3fb58a
icmauro <italo_mauro2000@yahoo.com.br>
2026-05-12 22:27:02 -0300

    primeiro comit

---
+++ ProEventos.Aula/Controllers/EventoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Aula.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Aula.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new List<Evento>();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return new List<Evento>();
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo de POST";
+        }
+    }
+}
