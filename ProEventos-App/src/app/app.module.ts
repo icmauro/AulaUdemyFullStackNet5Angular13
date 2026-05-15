c787b51e0f67c553ad7444979b64c5c67a1ea224
icmauro <italo_mauro2000@yahoo.com.br>
2026-05-15 15:21:57 -0300

    Sessão Interpolação Diretivas Binding

--- ProEventos-App/src/app/app.module.ts
+++ ProEventos-App/src/app/app.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,14 +10,52 @@ export class EventosComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 50;
+  heightImg: number = 60;
+
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
 
   public getEventos(): void {
 
     this.http.get('https://localhost:44368/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+           this.eventos = response;
+           this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
+
+  }
+
+  public get FiltroLista(): string
+  {
+    return this._filtroLista;
+  }
+
+  public set FiltroLista(value: string)
+  {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.FiltroLista ? this.FiltrarEventos(this.FiltroLista) : this.eventos;
+  }
+
+  public FiltrarEventos(filtrarPor: string): any
+  {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                                                   evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+
+  }
+
+  public AlternarImagem(): void {
+
+    this.mostrarImagem = !this.mostrarImagem;
 
   }
 
